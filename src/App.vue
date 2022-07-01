f074e84d36676954edd048f1040eb9044d70e720
silviawolves <silviacanepa.w@gmail.com>
2022-07-01 12:28:12 +0200

    navbar header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
+    
+    <TheHeader></TheHeader>
+    <TheMain></TheMain>
+    <TheFooter></TheFooter>
 
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue';
+import TheFooter from './components/TheFooter.vue';
 
 export default {
-  name: 'App',
-
+    name: "App",
+    components: { TheHeader, TheMain, TheFooter }
 }
 </script>
 
-<style lang="scss">
-@import "./assets/scss/main.scss";
+<style lang="scss" scoped>
 @import "./assets/scss/_variables.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: $primary;
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,61 @@
 <template>
-    <div></div>
+    <div class="container">
+        <div class="d-flex justify-content-between">
+
+            <!--LOGO PAGINA-->
+            <div class="page-logo">
+                <img src="/img/logo-dark.png" alt="logo landrick">
+            </div>
+
+            <!--LINK DI NAVIGAZIONE-->
+            <div class="page-navlink">
+                <ul class="list-unstyled d-flex">
+                    <li v-for="(link, i) in navLinks" :key="i"><a href="javascript:void(0)">{{ link }}</a></li>
+                </ul>
+            </div>
+
+            <!--FEATURES-->
+            <div class="page-features">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <button v-for="btn in features" :key="btn.name" class="btn btn-primary">
+                    <i :class="btn.icon"></i>
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'TheHeader',
+    data() {
+        return {
+            navLinks: ['Home', 'Landing', 'Pages', 'Docs'],
+            features: [
+                {
+                    name: 'GitHub',
+                    icon: 'fa-brands fa-github',
+                },
+                {
+                    name: 'Stack Overflow',
+                    icon: 'fa-brands fa-stack-overflow',
+                },
+                {
+                    name: 'User',
+                    icon: 'fa-regular fa-user',
+                }
+            ],
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/main.scss";
 @import "../assets/scss/_variables.scss";
+
+.page-logo img {
+    height: 20px;
+}
 
 </style>
