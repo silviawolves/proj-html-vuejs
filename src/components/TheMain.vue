bb72b3f1153de1784e4fe9af499e0a818b280e4a
silviawolves <silviacanepa.w@gmail.com>
2022-07-01 11:26:44 +0200

    commit base + img

---
+++ src/components/TheMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'TheMain',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/main.scss";
+@import "../assets/scss/_variables.scss";
+
+</style>
